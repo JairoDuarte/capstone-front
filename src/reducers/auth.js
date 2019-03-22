2aed07ad888f3044be571bac6b7f3b55ee575bb6
Jairo Duarte <alfredojairo17@hotmail.com>
2019-03-22 10:02:15 +0100

    fixed initial state in const variable.

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,6 +1,8 @@
 import * as ActionTypes from '../constants';
 
-export const Auth = (state = { errMess: null, location: {latitude: 0, longitude: 0}, user: JSON.parse(localStorage.getItem('user')), token: JSON.parse(localStorage.getItem('token')), isAuthenticated: JSON.parse(localStorage.getItem('isAuthenticated')) || false  }, action) => {
+const INITIAL_STATE =  { errMess: null, user: JSON.parse(localStorage.getItem('user')), token: JSON.parse(localStorage.getItem('token')), isAuthenticated: JSON.parse(localStorage.getItem('isAuthenticated')) || false  };
+
+export const Auth = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case ActionTypes.SIGN_IN:
             return { ...state, user: action.payload.user, token: action.payload.token, isAuthenticated: true };
