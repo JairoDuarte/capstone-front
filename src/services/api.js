3c70bf0218b244ff6205c52ffa8874583b91e9a9
Jairo Duarte <alfredojairo17@hotmail.com>
2019-08-22 11:22:49 +0100

    Update get token in localstorage

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
-import axios from "axios";
-import {baseUrl} from './baseUrl';
+import axios from 'axios';
+import { baseUrl } from './baseUrl';
 const api = axios.create({
   baseURL: baseUrl
 });
 
 api.interceptors.request.use(async config => {
-  const token = JSON.parse(localStorage.getItem('token'));
+  const token = localStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
