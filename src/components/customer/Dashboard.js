412f69dfc247bc65aed1da5e73f9efec0c441cff
Jairo Duarte <alfredojairo17@hotmail.com>
2019-08-22 11:24:45 +0100

    Update * prettier

--- src/components/customer/Dashboard.js
+++ src/components/customer/Dashboard.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
-import { Container, Grid, TransitionablePortal, Button, Icon, Segment  } from 'semantic-ui-react';
-import { withRouter, Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Container, Grid, TransitionablePortal, Button, Icon, Segment } from 'semantic-ui-react';
+import { withRouter, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import Menu from './Menu'
+import Menu from './Menu';
 import Profile from './Profile';
 import { updateUserService, removeNotification, addNotification } from '../../actions/user';
 import { addSkheraService, getSkheraService } from '../../actions/skhera';
@@ -10,176 +10,210 @@ import RequestSkhera from './RequestSkhera';
 import MySkhera from './MySkhera';
 
 const mapStateToProps = state => {
-    return {
-        user: state.auth.user,
-        notification: state.user.notification,
-        open: state.user.open,
-        skheras: state.skhera.skheras,
-        errMess: state.user.errMess,
-        errMessSkhera: state.skhera.errMess
-    }
-}
-
+  return {
+    user: state.auth.user,
+    notification: state.user.notification,
+    open: state.user.open,
+    skheras: state.skhera.skheras,
+    errMess: state.user.errMess,
+    errMessSkhera: state.skhera.errMess
+  };
+};
 
 const mapDispatchToProps = dispatch => ({
-    updateUserService: (user) => dispatch(updateUserService(user)),
-    addNotification: (notification) => dispatch(addNotification(notification)),
-    removeNotification: () => dispatch(removeNotification()),
-    addSkheraService: (skhera) => dispatch(addSkheraService(skhera)),
-    getSkheraService: () => dispatch(getSkheraService())
-
+  updateUserService: user => dispatch(updateUserService(user)),
+  addNotification: notification => dispatch(addNotification(notification)),
+  removeNotification: () => dispatch(removeNotification()),
+  addSkheraService: skhera => dispatch(addSkheraService(skhera)),
+  getSkheraService: () => dispatch(getSkheraService())
 });
 
-
 function ButtomRequest() {
-    return (
-        <Button as={Link} to='/skhera/2/add' size='huge' color='grs' style={{ textAlign: 'left', width: '252px', marginLeft: '1.7em', padding: '1em 0px' }}>
-            <Icon name='edit outline' size='big' style={{ marginTop: '1em', marginBottom: '1em', marginLeft: '1.0em' }} />
-            <br /><span style={{ padding: '0px 30px', height: '19px', width: '148px', color: '#FFFFFF', fontFamily: 'Ropa Sans', fontSize: '18px', lineheight: '19px', marginBottom: '' }}>  Request Skhera</span>
-            <span style={{float: 'right', fontWeight: 'normal',  }}> <Icon name='right arrow' style={{ marginRight: '10px', padding: '0px 0px' }} />
-            </span>
-        </Button>
-    )
+  return (
+    <Button
+      as={Link}
+      to="/skhera/2/add"
+      size="huge"
+      color="grs"
+      style={{ textAlign: 'left', width: '252px', marginLeft: '1.7em', padding: '1em 0px' }}
+    >
+      <Icon name="edit outline" size="big" style={{ marginTop: '1em', marginBottom: '1em', marginLeft: '1.0em' }} />
+      <br />
+      <span
+        style={{
+          padding: '0px 30px',
+          height: '19px',
+          width: '148px',
+          color: '#FFFFFF',
+          fontFamily: 'Ropa Sans',
+          fontSize: '18px',
+          lineheight: '19px',
+          marginBottom: ''
+        }}
+      >
+        {' '}
+        Request Skhera
+      </span>
+      <span style={{ float: 'right', fontWeight: 'normal' }}>
+        {' '}
+        <Icon name="right arrow" style={{ marginRight: '10px', padding: '0px 0px' }} />
+      </span>
+    </Button>
+  );
 }
 
 class Dashboard extends Component {
-    state = { active: 'My Profile', open: false, columns: 3, menus: [{ label: 'My Skhera', url: '/skhera/2/list' }, { label: 'My Profile', url: '/profile/3' }, { label: 'My Address', url: '/profile/3/address' }, { label: 'FAQ', url: '/profile/2/faq' }] }
-    componentDidMount() {
-        const { socket } = this.props;
-        if(this.props.match.params.page === 'list') this.props.getSkheraService();
-        
-        socket.on('accept skhera', ({ idconsumer }) => {
-            if (idconsumer === this.props.user.id) {
-                this.props.addNotification('Your skhera is accepted by Rider');
-            }
-        })
-    }
-    render() {
-        const { params } = this.props.match;
+  state = {
+    active: 'My Profile',
+    open: false,
+    columns: 3,
+    menus: [
+      { label: 'My Skhera', url: '/skhera/2/list' },
+      { label: 'My Profile', url: '/profile/3' },
+      { label: 'My Address', url: '/profile/3/address' },
+      { label: 'FAQ', url: '/profile/2/faq' }
+    ]
+  };
+  componentDidMount() {
+    const { socket } = this.props;
+    if (this.props.match.params.page === 'list') this.props.getSkheraService();
 
-        const RenderMenu = () => {
-           if (!params.page) {
-                return (
-                    <Menu active='My Profile' menus={this.state.menus}></Menu>
-                )
-            }
-            else if (params.page === 'list') {
-                return (
-                    <Menu active='My Skhera' menus={this.state.menus}></Menu>
-                )
-            }
-            else if (params.page === 'add') {
-                return (
-                    <Menu active='My Skhera' menus={this.state.menus}></Menu>
-                )
-            }
-            else if (params.page === 'address') {
-                return (
-                    <Menu active='My Address' menus={this.state.menus}></Menu>
-                )
-            }
-            else if (params.page === 'faq') {
-                return (
-                    <Menu active='FAQ' menus={this.state.menus}></Menu>
-                )
-            }
-        }
-        const RenderComponent = () => {
-            if (!this.props.match.params.page) {
-                return (
-                    <>
-                        <Grid.Column textAlign='left'>
-                            <Profile mobile={this.props.mobile} errMess={this.props.errMess} updateUserService={this.props.updateUserService} user={this.props.user}></Profile>
-                        </Grid.Column>
-                        {this.props.mobile ? <></> :
-                        <Grid.Column>
-                             <ButtomRequest></ButtomRequest>
-                        </Grid.Column>
-                        }
-                    </>
-                )
-            }
-            else if (this.props.match.params.page === 'list') {
+    socket.on('accept skhera', ({ idconsumer }) => {
+      if (idconsumer === this.props.user.id) {
+        this.props.addNotification('Your skhera is accepted by Rider');
+      }
+    });
+  }
+  render() {
+    const { params } = this.props.match;
 
-                return (<>
-                    <Grid.Column width={11} textAlign='left' >
-                        <MySkhera getSkheraService={this.props.getSkheraService} errMessSkhera={this.props.errMessSkhera}  skheras={this.props.skheras}></MySkhera>
-                    </Grid.Column>
-                    <Grid.Column>
-
-                    </Grid.Column>
-                </>)
-            }
-            else if (this.props.match.params.page === 'address') {
-
-                return (
-                    <>
-                        <Grid.Column textAlign='left'>
-                        </Grid.Column>
-                        {this.props.mobile ? <></> :
-                        <Grid.Column>
-                             <ButtomRequest></ButtomRequest>
-                        </Grid.Column>
-                        }
-                    </>
-                )
-            }
-            else if (this.props.match.params.page === 'faq') {
-
-                return (
-                    <>
-                        <Grid.Column textAlign='left'>
-                           
-                        </Grid.Column>
-
-                    </>
-                )
-            }
-            else if (this.props.match.params.page === 'add') {
-
-                return (
-                    <>
-                        <Grid.Column textAlign='left'>
-                            <RequestSkhera errMessSkhera={this.props.errMessSkhera} addSkheraService={this.props.addSkheraService}></RequestSkhera>
-                        </Grid.Column>
-
-                    </>
-                )
-            }
-        }
-        const width = () => params.page === 'add' ? 8 : 4
-
+    const RenderMenu = () => {
+      if (!params.page) {
+        return <Menu active="My Profile" menus={this.state.menus} />;
+      } else if (params.page === 'list') {
+        return <Menu active="My Skhera" menus={this.state.menus} />;
+      } else if (params.page === 'add') {
+        return <Menu active="My Skhera" menus={this.state.menus} />;
+      } else if (params.page === 'address') {
+        return <Menu active="My Address" menus={this.state.menus} />;
+      } else if (params.page === 'faq') {
+        return <Menu active="FAQ" menus={this.state.menus} />;
+      }
+    };
+    const RenderComponent = () => {
+      if (!this.props.match.params.page) {
         return (
-            <div>
-                <Container  style={{ color: 'black', marginBottom: '150px', marginTop: '120px', width: '100%', height: '100%', padding: '0em 0em' }} >
-                    <Grid  columns={this.props.mobile ? 1 : this.props.match.params.columns} container >
-                        <Grid.Row>
-                            {this.props.mobile  ? <></> :
-                            <Grid.Column width={width()} style={{ marginTop: '1.2em' }}>
-                                <RenderMenu></RenderMenu>
-                            </Grid.Column>
-                            }
-                            <RenderComponent></RenderComponent>
-                        </Grid.Row>
-                        <TransitionablePortal transition={{duration: 1000}} onClose={()=> this.props.removeNotification()} open={this.props.open}>
-                            <Segment
-                                raised={true}
-                                
-                                style={{
-                                    left: '57%',
-                                    position: 'absolute',
-                                    top: '15%',
-                                    zIndex: 1000,
-                                }}
-                            >
-                            <h4 style={{ fontWeight: 'normal'  }}>{this.props.notification}</h4>
+          <>
+            <Grid.Column textAlign="left">
+              <Profile
+                mobile={this.props.mobile}
+                errMess={this.props.errMess}
+                updateUserService={this.props.updateUserService}
+                user={this.props.user}
+              />
+            </Grid.Column>
+            {this.props.mobile ? (
+              <></>
+            ) : (
+              <Grid.Column>
+                <ButtomRequest />
+              </Grid.Column>
+            )}
+          </>
+        );
+      } else if (this.props.match.params.page === 'list') {
+        return (
+          <>
+            <Grid.Column width={11} textAlign="left">
+              <MySkhera
+                getSkheraService={this.props.getSkheraService}
+                errMessSkhera={this.props.errMessSkhera}
+                skheras={this.props.skheras}
+              />
+            </Grid.Column>
+            <Grid.Column />
+          </>
+        );
+      } else if (this.props.match.params.page === 'address') {
+        return (
+          <>
+            <Grid.Column textAlign="left" />
+            {this.props.mobile ? (
+              <></>
+            ) : (
+              <Grid.Column>
+                <ButtomRequest />
+              </Grid.Column>
+            )}
+          </>
+        );
+      } else if (this.props.match.params.page === 'faq') {
+        return (
+          <>
+            <Grid.Column textAlign="left" />
+          </>
+        );
+      } else if (this.props.match.params.page === 'add') {
+        return (
+          <>
+            <Grid.Column textAlign="left">
+              <RequestSkhera errMessSkhera={this.props.errMessSkhera} addSkheraService={this.props.addSkheraService} />
+            </Grid.Column>
+          </>
+        );
+      }
+    };
+    const width = () => (params.page === 'add' ? 8 : 4);
 
-                            </Segment>
-                        </TransitionablePortal>
-                    </Grid>
-                </Container>
-            </div>
-        )
-    }
+    return (
+      <div>
+        <Container
+          style={{
+            color: 'black',
+            marginBottom: '150px',
+            marginTop: '120px',
+            width: '100%',
+            height: '100%',
+            padding: '0em 0em'
+          }}
+        >
+          <Grid columns={this.props.mobile ? 1 : this.props.match.params.columns} container>
+            <Grid.Row>
+              {this.props.mobile ? (
+                <></>
+              ) : (
+                <Grid.Column width={width()} style={{ marginTop: '1.2em' }}>
+                  <RenderMenu />
+                </Grid.Column>
+              )}
+              <RenderComponent />
+            </Grid.Row>
+            <TransitionablePortal
+              transition={{ duration: 1000 }}
+              onClose={() => this.props.removeNotification()}
+              open={this.props.open}
+            >
+              <Segment
+                raised={true}
+                style={{
+                  left: '57%',
+                  position: 'absolute',
+                  top: '15%',
+                  zIndex: 1000
+                }}
+              >
+                <h4 style={{ fontWeight: 'normal' }}>{this.props.notification}</h4>
+              </Segment>
+            </TransitionablePortal>
+          </Grid>
+        </Container>
+      </div>
+    );
+  }
 }
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Dashboard));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Dashboard)
+);
--- src/components/rider/Dashboard.js
+++ src/components/rider/Dashboard.js
@@ -1,172 +1,185 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Container, Grid, TransitionablePortal, Segment } from 'semantic-ui-react';
-import { withRouter } from 'react-router-dom'
+import { withRouter } from 'react-router-dom';
 import { geolocated } from 'react-geolocated';
 import { connect } from 'react-redux';
-import Menu from '../customer/Menu'
+import Menu from '../customer/Menu';
 import Profile from '../customer/Profile';
 import { updateUserService, addNotification, removeNotification, addLocation } from '../../actions/user';
 import { notificationSkheraService } from '../../actions/skhera';
 import ErrorMessage from '../ErrorMessage';
 
 const mapStateToProps = state => {
-    return {
-        user: state.auth.user,
-        notification: state.user.notification,
-        open: state.user.open,
-        location: state.user.location,
-        errMess: state.user.errMess,
-    }
-}
+  return {
+    user: state.auth.user,
+    notification: state.user.notification,
+    open: state.user.open,
+    location: state.user.location,
+    errMess: state.user.errMess
+  };
+};
 
 const mapDispatchToProps = dispatch => ({
-    updateUserService: (user) => dispatch(updateUserService(user)),
-    addNotification: (notification) => dispatch(addNotification(notification)),
-    removeNotification: () => dispatch(removeNotification()),
-    addLocation: (location) => dispatch(addLocation(location)),
-    notificationSkheraService: (notification) => dispatch(notificationSkheraService(notification))
-
+  updateUserService: user => dispatch(updateUserService(user)),
+  addNotification: notification => dispatch(addNotification(notification)),
+  removeNotification: () => dispatch(removeNotification()),
+  addLocation: location => dispatch(addLocation(location)),
+  notificationSkheraService: notification => dispatch(notificationSkheraService(notification))
 });
 
 class Dashboard extends Component {
-    state = { active: 'My Profile', columns: 3, menus: [{ label: 'Skherat TODO', url: '/dashboard/skhera/2/' }, { label: 'My Profile', url: '/dashboard/profile/3' }, { label: 'Statistics', url: '/dashboard/statistics/3/' }, { label: 'FAQ', url: '/dashboard/faq/2/' }] }
+  state = {
+    active: 'My Profile',
+    columns: 3,
+    menus: [
+      { label: 'Skherat TODO', url: '/dashboard/skhera/2/' },
+      { label: 'My Profile', url: '/dashboard/profile/3' },
+      { label: 'Statistics', url: '/dashboard/statistics/3/' },
+      { label: 'FAQ', url: '/dashboard/faq/2/' }
+    ]
+  };
 
-    componentDidMount() {
-        const { socket } = this.props;
-        this.handleLocation = setInterval(() => this.setLocation(), 1000000);
+  componentDidMount() {
+    const { socket } = this.props;
+    this.handleLocation = setInterval(() => this.setLocation(), 1000000);
 
-        socket.on('new skhera', ({ skhera, idrider }) => {
-
-            if (idrider === this.props.user.id) {
-                let notifications = JSON.parse(localStorage.getItem('notifications')) || [];
-                for (let index = 0; index < notifications.length; index++) {
-                    const element = notifications[index];
-                    if (element._id === skhera._id) {
-                        return null;
-                    }
-                }
-                this.props.notificationSkheraService(skhera);
-            }
-        })
+    socket.on('new skhera', ({ skhera, idrider }) => {
+      if (idrider === this.props.user.id) {
+        let notifications = JSON.parse(localStorage.getItem('notifications')) || [];
+        for (let index = 0; index < notifications.length; index++) {
+          const element = notifications[index];
+          if (element._id === skhera._id) {
+            return null;
+          }
+        }
+        this.props.notificationSkheraService(skhera);
+      }
+    });
+  }
+  setLocation = () => {
+    if (
+      this.props.isGeolocationEnabled &&
+      this.props.coords &&
+      (this.props.coords.latitude.toPrecision(4) !== this.props.location.latitude.toPrecision(4) ||
+        this.props.coords.longitude.toPrecision(4) !== this.props.location.longitude.toPrecision(4))
+    ) {
+      const { socket } = this.props;
+      socket.emit('location', {
+        location: {
+          longitude: this.props.coords.longitude,
+          latitude: this.props.coords.latitude
+        },
+        id: this.props.user.id
+      });
+      this.props.addLocation({
+        longitude: this.props.coords.longitude,
+        latitude: this.props.coords.latitude
+      });
+      this.setState({
+        longitude: this.props.coords.longitude,
+        latitude: this.props.coords.latitude
+      });
     }
-    setLocation = () => {
-        if (this.props.isGeolocationEnabled && this.props.coords && (this.props.coords.latitude.toPrecision(4) !== this.props.location.latitude.toPrecision(4) || this.props.coords.longitude.toPrecision(4) !== this.props.location.longitude.toPrecision(4))) {
-            const { socket } = this.props;
-            socket.emit('location', { location: { longitude: this.props.coords.longitude, latitude: this.props.coords.latitude }, id: this.props.user.id });
-            this.props.addLocation({ longitude: this.props.coords.longitude, latitude: this.props.coords.latitude });
-            this.setState({ longitude: this.props.coords.longitude, latitude: this.props.coords.latitude });
-        }
-    }
+  };
 
-    render() {
-        const { params } = this.props.match;
+  render() {
+    const { params } = this.props.match;
 
-        const RenderMenu = () => {
-
-            if (params.page === 'profile') {
-                return (
-                    <Menu active='My Profile' menus={this.state.menus}></Menu>
-                )
-            }
-            else if (params.page === 'skhera') {
-                return (
-                    <Menu active='Skherat TODO' menus={this.state.menus}></Menu>
-                )
-            }
-            else if (params.page === 'statistics') {
-                return (
-                    <Menu active='Statistics' menus={this.state.menus}></Menu>
-                )
-            }
-            else if (params.page === 'faq') {
-                return (
-                    <Menu active='FAQ' menus={this.state.menus}></Menu>
-                )
-            }
-        }
-        const RenderComponent = () => {
-            if (params.page === 'profile') {
-                return (
-                    <>
-                        <Grid.Column textAlign='left'>
-                            <Profile updateUserService={this.props.updateUserService} user={this.props.user}></Profile>
-                        </Grid.Column>
-                        <Grid.Column>
-                        </Grid.Column>
-                    </>
-                )
-            }
-            else if (params.page === 'skhera') {
-
-                return (
-                    <>
-                    </>
-                )
-            }
-            else if (params.page === 'statistics') {
-
-                return (
-                    <>
-                        <Grid.Column textAlign='left'>
-                        </Grid.Column>
-                        <Grid.Column>
-                        </Grid.Column>
-                    </>
-                )
-            }
-            else if (params.page === 'faq') {
-
-                return (
-                    <>
-                        <Grid.Column textAlign='left'>
-                        </Grid.Column>
-
-                    </>
-                )
-            }
-        }
-
+    const RenderMenu = () => {
+      if (params.page === 'profile') {
+        return <Menu active="My Profile" menus={this.state.menus} />;
+      } else if (params.page === 'skhera') {
+        return <Menu active="Skherat TODO" menus={this.state.menus} />;
+      } else if (params.page === 'statistics') {
+        return <Menu active="Statistics" menus={this.state.menus} />;
+      } else if (params.page === 'faq') {
+        return <Menu active="FAQ" menus={this.state.menus} />;
+      }
+    };
+    const RenderComponent = () => {
+      if (params.page === 'profile') {
         return (
-            <div>
-                <Container style={{ color: 'black', marginBottom: '150px', marginTop: '120px', width: '100%', height: '100%', padding: '0em 0em' }} >
-                    {this.props.errMess ?
-                        <ErrorMessage header={this.props.errMess} message='Retry the action' ></ErrorMessage> :
-                        <></>
-                    }
-                    <Grid columns={this.props.match.params.columns} container stackable  >
-                        <Grid.Row>
-                            <Grid.Column style={{ marginTop: '1.2em' }}>
-                                <RenderMenu></RenderMenu>
-                            </Grid.Column>
-                            <RenderComponent></RenderComponent>
-                        </Grid.Row>
-                        <TransitionablePortal transition={{ duration: 1000 }} onClose={() => this.props.removeNotification()} open={this.props.open}>
-                            <Segment
-                                raised={true}
+          <>
+            <Grid.Column textAlign="left">
+              <Profile updateUserService={this.props.updateUserService} user={this.props.user} />
+            </Grid.Column>
+            <Grid.Column />
+          </>
+        );
+      } else if (params.page === 'skhera') {
+        return <></>;
+      } else if (params.page === 'statistics') {
+        return (
+          <>
+            <Grid.Column textAlign="left" />
+            <Grid.Column />
+          </>
+        );
+      } else if (params.page === 'faq') {
+        return (
+          <>
+            <Grid.Column textAlign="left" />
+          </>
+        );
+      }
+    };
 
-                                style={{
-                                    left: '62%',
-                                    position: 'absolute',
-                                    top: '15%',
-                                    zIndex: 1000,
-                                }}
-                            >
-                                <h4 style={{ fontWeight: 'normal' }}>{this.props.notification}</h4>
-
-                            </Segment>
-                        </TransitionablePortal>
-                    </Grid>
-                </Container>
-            </div>
-        )
-    }
+    return (
+      <div>
+        <Container
+          style={{
+            color: 'black',
+            marginBottom: '150px',
+            marginTop: '120px',
+            width: '100%',
+            height: '100%',
+            padding: '0em 0em'
+          }}
+        >
+          {this.props.errMess ? <ErrorMessage header={this.props.errMess} message="Retry the action" /> : <></>}
+          <Grid columns={this.props.match.params.columns} container stackable>
+            <Grid.Row>
+              <Grid.Column style={{ marginTop: '1.2em' }}>
+                <RenderMenu />
+              </Grid.Column>
+              <RenderComponent />
+            </Grid.Row>
+            <TransitionablePortal
+              transition={{ duration: 1000 }}
+              onClose={() => this.props.removeNotification()}
+              open={this.props.open}
+            >
+              <Segment
+                raised={true}
+                style={{
+                  left: '62%',
+                  position: 'absolute',
+                  top: '15%',
+                  zIndex: 1000
+                }}
+              >
+                <h4 style={{ fontWeight: 'normal' }}>{this.props.notification}</h4>
+              </Segment>
+            </TransitionablePortal>
+          </Grid>
+        </Container>
+      </div>
+    );
+  }
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(geolocated({
-    positionOptions: {
-        enableHighAccuracy: false,
-    },
-    userDecisionTimeout: 5000,
-})(Dashboard)));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(
+    geolocated({
+      positionOptions: {
+        enableHighAccuracy: false
+      },
+      userDecisionTimeout: 5000
+    })(Dashboard)
+  )
+);
 
 //export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Dashboard));
